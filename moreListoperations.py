0346ea4f8d4ec4ec5a918413277cab888992a93d
Shohanur Rahman <shohanurvu@gmail.com>
2020-06-08 21:14:32 +0600

    more operations in list

--- moreListoperations.py
+++ moreListoperations.py
@@ -1,5 +1,21 @@
-mondey_temparature = [9.1 , 8.8, 7.5]
+mondey_temparature = [9.1 , 8.8,'hello', 7.5,3.4,9.2]
 
 
-mondey_temparature.append(3.2)
- 
+
+a = mondey_temparature.__getitem__(1)
+print(a)
+b = mondey_temparature[3]
+print(b) 
+print(len(mondey_temparature))
+c= mondey_temparature[3:5]
+print(c)
+
+d= mondey_temparature[-1]
+mystring  = 'hello'
+
+l = mondey_temparature[2][2]
+
+eng_bangla = {"rain":"bristy", "sun":"surjo"} 
+
+y = eng_bangla["rain"] 
+print(y)
