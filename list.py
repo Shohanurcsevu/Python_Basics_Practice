883557d00af8fef84410ded45666083af43937d5
Shohanur Rahman <shohanurvu@gmail.com>
2020-06-08 11:39:43 +0600

    python dictionary

---
+++ list.py
@@ -0,0 +1,9 @@
+monday_temperatures = [9.1 ,1.5,5.8] 
+student_grades = {"Marray": 9.1, "jhon": 8.5, "kaile": 5.5}
+
+
+mysum=sum(student_grades.values())
+mylen=len(student_grades.values())
+myaveg=mysum / mylen
+
+print(mysum,mylen,myaveg)
