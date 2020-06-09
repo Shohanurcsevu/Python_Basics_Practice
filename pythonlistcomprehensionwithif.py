6eae3eca3a38f917413faefbbfcde4346676329f
Shohanur Rahman <shohanurvu@gmail.com>
2020-06-09 11:59:24 +0600

    witth if else condition

--- pythonlistcomprehensionwithif.py
+++ pythonlistcomprehensionwithif.py
@@ -1,3 +1,5 @@
 temps = [ 222, 231, 233, -999, 234]
-new_temps = [temp  /10 for temp in temps if temp !=-999]
-print(new_temps)
+new_temps1 = [temp  /10 for temp in temps if temp !=-999]
+new_temps2 = [temp / 10 if temp !=-999 else 0 for temp in temps]
+print(new_temps1)
+print(new_temps2)
