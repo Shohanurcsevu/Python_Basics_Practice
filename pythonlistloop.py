10ec619a7f7fd61e9f23587d37a662b6247b6969
Shohanur Rahman <shohanurvu@gmail.com>
2020-06-09 11:49:40 +0600

    python list comprehensions

---
+++ pythonlistloop.py
@@ -0,0 +1,10 @@
+temps = [ 221, 234, 212 ]
+
+new_temps = []
+for temp in temps:
+    new_temps.append(temp/10)
+    
+    
+print(new_temps)
+new_tempss = [temp/10 for temp in temps]
+print(new_tempss)
